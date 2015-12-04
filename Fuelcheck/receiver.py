5f48f240e534c369c5698c0338f45e83ca0195f1
henry-spinitalia <e.dabramo@spinitalia.com>
2015-12-04 13:08:20 +0100

    Aggiornato il receiver con log

--- Fuelcheck/receiver.py
+++ Fuelcheck/receiver.py
@@ -16,53 +16,67 @@ from twisted.internet import reactor
 import binascii
 import time
 import os
+import sys
 from ControlUnit import ControlUnit
+
 
 class BBoxDecoder(DatagramProtocol):
     """Riceve un messaggio compresso dalle centraline e lo spedisce al server"""
 
     C_TXS_URL = "http://151.1.80.42/src/api/v1?rawData="
+    C_OUT_PATH = "/mnt/nas/temp/"
     C_OUT_FILE = "/mnt/nas/temp/fuelcheck_traslator.log"
 
     def __init__(self):
 
         self.ctrl_unit = ControlUnit()
 
-        if os.path.isdir(C_OUT_FILE):
+        if os.path.isdir(self.C_OUT_PATH):
             self.can_log = True
+            print("Log path found")
+        else:
+            self.can_log = False
+            print("Log path not found")
 
         print "Starting ControlUnit binary receiver v0.1a"
 
+    def log_data(self, date, imei, hex_data, host, port, comment):
+
+        if self.can_log:
+            with open(self.C_OUT_FILE, 'a') as f:
+                f.write(
+                    "{0};{1};{2};{3};{4};{5}\n".format(
+                        date,
+                        imei,
+                        hex_data,
+                        host,
+                        port,
+                        comment
+                    )
+                )
+
     def print_page(self, result, host, port, data, ctrl_unit):
         print "  MULE say {}".format(result)
-        if self.can_log:
-                with open(self.C_OUT_FILE, 'w+') as f:
-                    f.write(
-                        "{0};{1};{2};{3};{4};{5}".format(
-                            time.strftime("%d %b %H:%M:%S", time.gmtime(ctrl_unit.unixtime)),
-                            ctrl_unit.imei,
-                            binascii.hexlify(data).upper(),
-                            host,
-                            port,
-                            result
-                        )
-                    )
+        self.log_data(
+            time.strftime("%d %b %H:%M:%S", time.gmtime(ctrl_unit.unixtime)),
+            ctrl_unit.imei,
+            binascii.hexlify(data).upper(),
+            host,
+            port,
+            result
+        )
         self.transport.write(result, (host, port))
 
     def print_error(self, failure, host, port, data, ctrl_unit):
         print "  MULE say EPIC FAIL! {} - {}".format(sys.stderr, failure)
-        if self.can_log:
-                with open(self.C_OUT_FILE, 'w+') as f:
-                    f.write(
-                        "{0};{1};{2};{3};{4};{5}".format(
-                            time.strftime("%d %b %H:%M:%S", time.gmtime(ctrl_unit.unixtime)),
-                            ctrl_unit.imei,
-                            binascii.hexlify(data).upper(),
-                            host,
-                            port,
-                            failure
-                        )
-                    )
+        self.log_data(
+            time.strftime("%d %b %H:%M:%S", time.gmtime(ctrl_unit.unixtime)),
+            ctrl_unit.imei,
+            binascii.hexlify(data).upper(),
+            host,
+            port,
+            failure
+        )
 
     def datagramReceived(self, data, (host, port)):
 
@@ -72,89 +86,70 @@ class BBoxDecoder(DatagramProtocol):
         # La decodifico da binario in variabili interne
         try:
             self.ctrl_unit.decode_binary(data)
-        except:
+        except (ValueError, TypeError):
             print("  - Conversion error")
-            if self.can_log:
-                with open(self.C_OUT_FILE, 'w+') as f:
-                    f.write(
-                        "{0};{1};{2};{3};{4};{5}".format(
-                            "",
-                            "",
-                            binascii.hexlify(data).upper(),
-                            host,
-                            port,
-                            "Conversion error"
-                        )
-                    )
+            self.log_data(
+                "",
+                "",
+                binascii.hexlify(data).upper(),
+                host,
+                port,
+                "Conversion error"
+            )
 
         # Se i dati sono validi
         if self.ctrl_unit.check_values():
 
-            if self.can_log:
-                with open(self.C_OUT_FILE, 'w+') as f:
-                    f.write(
-                        "{0};{1};{2};{3};{4};{5}".format(
-                            time.strftime("%d %b %H:%M:%S", time.gmtime(self.ctrl_unit.unixtime)),
-                            self.ctrl_unit.imei,
-                            binascii.hexlify(data).upper(),
-                            host,
-                            port,
-                            "OK"
-                        )
-                    )
+            self.log_data(
+                time.strftime("%d %b %H:%M:%S", time.gmtime(self.ctrl_unit.unixtime)),
+                self.ctrl_unit.imei,
+                binascii.hexlify(data).upper(),
+                host,
+                port,
+                "OK"
+            )
 
             # Li codifico in ASCII
             try:
                 self.ctrl_unit.encode_ascii()
-            except:
+            except (ValueError, TypeError):
                 print("  - Encoding error")
-                if self.can_log:
-                with open(self.C_OUT_FILE, 'w+') as f:
-                    f.write(
-                        "{0};{1};{2};{3};{4};{5}".format(
-                            "",
-                            "",
-                            binascii.hexlify(data).upper(),
-                            host,
-                            port,
-                            "Encoding error"
-                        )
-                    )
+                self.log_data(
+                    "",
+                    "",
+                    binascii.hexlify(data).upper(),
+                    host,
+                    port,
+                    "Encoding error"
+                )
 
             print("  Coded {}".format(self.ctrl_unit.output_packet))
-            if self.can_log:
-                with open(self.C_OUT_FILE, 'w+') as f:
-                    f.write(
-                        "{0};{1};{2};{3};{4};{5}".format(
-                            time.strftime("%d %b %H:%M:%S", time.gmtime(self.ctrl_unit.unixtime)),
-                            self.ctrl_unit.imei,
-                            binascii.hexlify(data).upper(),
-                            host,
-                            port,
-                            self.ctrl_unit.output_packet
-                        )
-                    )
+            self.log_data(
+                time.strftime("%d %b %H:%M:%S", time.gmtime(self.ctrl_unit.unixtime)),
+                self.ctrl_unit.imei,
+                binascii.hexlify(data).upper(),
+                host,
+                port,
+                self.ctrl_unit.output_packet
+            )
 
             # Li invio al server MULE, e quando sarà pronta la risposta faccio chiamare printPage o printError
             d = getPage(self.C_TXS_URL + self.ctrl_unit.output_packet)
-            d.addCallback(self.print_page, host, port, data, ctrl_unit)
-            d.addErrback(self.print_error, host, port, data, ctrl_unit)
+            d.addCallback(self.print_page, host, port, data, self.ctrl_unit)
+            d.addErrback(self.print_error, host, port, data, self.ctrl_unit)
             # d.addBoth(stop)
 
         else:
-            if self.can_log:
-                with open(self.C_OUT_FILE, 'w+') as f:
-                    f.write(
-                        "{0};{1};{2};{3};{4};{5}".format(
-                            "",
-                            "",
-                            binascii.hexlify(data).upper(),
-                            host,
-                            port,
-                            "Value error"
-                        )
-                    )
+            self.log_data(
+                "",
+                "",
+                binascii.hexlify(data).upper(),
+                host,
+                port,
+                "Value error"
+            )
 
 if __name__ == '__main__':
+
     reactor.listenUDP(9123, BBoxDecoder())
     reactor.run()
